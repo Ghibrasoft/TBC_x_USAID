ce79496c738d03e47bbdd95a3059e14e1cf132f0
David Ghibradze <96905686+Ghibrasoft@users.noreply.github.com>
2024-01-22 22:18:12 +0400

    Fixing: resp design

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// ---------- get window width ---------- //
+// ---------- GET WINDOW WIDTH ---------- //
 var windowWidth =
   window.innerWidth ||
   document.documentElement.clientWidth ||
@@ -13,7 +13,7 @@ function updateWindowWidth() {
 updateWindowWidth();
 window.addEventListener("resize", updateWindowWidth);
 
-// ---------- change header bg onScroll ---------- //
+// ---------- CHANGE HEADER BG ONSCROLL ---------- //
 const header = document.getElementById("header");
 function changeHeaderBg() {
   const scrollPosition = window.scrollY || document.documentElement.scrollTop;
@@ -26,7 +26,7 @@ function changeHeaderBg() {
 }
 window.addEventListener("scroll", changeHeaderBg);
 
-// ---------- active nav tabs ---------- //
+// ---------- ACTIVE NAVIGATION TABS ---------- //
 const navLinks = document.querySelectorAll(".nav-link");
 navLinks.forEach((navLink) => {
   navLink.addEventListener("click", () => {
@@ -36,7 +36,7 @@ navLinks.forEach((navLink) => {
   });
 });
 
-// ---------- burger menu ---------- //
+// ---------- BURGER MENU ---------- //
 // const burgerMenu = document.querySelector(".burger-menu-wrapper-inner");
 // const burgerTopLine = document.querySelector(".top-line");
 // const burgerBottomLine = document.querySelector(".bottom-line");
@@ -48,136 +48,203 @@ navLinks.forEach((navLink) => {
 //   burgerBottomLine.classList.toggle("bottom-line-rotate");
 // });
 
-// ---------- dynamically append learning courses ---------- //
-fetch("./db/cards-data.json")
-  .then((res) => res.json())
-  .then((cardsData) => {
-    const gridContainer = document.getElementById("grid-cards");
+// ---------- LEARNING COURSES ---------- //
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("./db/cards-data.json")
+    .then((res) => res.json())
+    .then((cardsData) => {
+      const gridContainer = document.getElementById("grid-cards");
 
-    cardsData.forEach((article) => {
-      const card = document.createElement("article");
-      const cardCover = document.createElement("img");
-      const cardContentWrapper = document.createElement("div");
-      const cardActionWrapper = document.createElement("div");
-      const cardTitleWrapper = document.createElement("div");
-      const cardAction = document.createElement("a");
-      const arrowIcon = document.createElement("img");
+      cardsData.forEach((article) => {
+        const card = document.createElement("article");
+        const cardCover = document.createElement("img");
+        const cardContentWrapper = document.createElement("div");
+        const cardActionWrapper = document.createElement("div");
+        const cardTitleWrapper = document.createElement("div");
+        const cardAction = document.createElement("a");
+        const arrowIcon = document.createElement("img");
 
-      card.className = "second-section-grid-container-card";
-      cardContentWrapper.className =
-        "second-section-grid-container-card-content";
-      cardCover.className = "second-section-grid-container-card-cover";
-      cardActionWrapper.className =
-        "second-section-grid-container-card-action-wrapper";
-      cardTitleWrapper.className =
-        "second-section-grid-container-card-title-wrapper";
-      cardAction.className = "second-section-grid-container-card-action";
+        card.className = "second-section-grid-container-card";
+        cardContentWrapper.className =
+          "second-section-grid-container-card-content";
+        cardCover.className = "second-section-grid-container-card-cover";
+        cardActionWrapper.className =
+          "second-section-grid-container-card-action-wrapper";
+        cardTitleWrapper.className =
+          "second-section-grid-container-card-title-wrapper";
+        cardAction.className = "second-section-grid-container-card-action";
 
-      cardCover.alt = "card-cover";
-      cardCover.src = article.image;
-      card.appendChild(cardCover);
+        cardCover.alt = "card-cover";
+        cardCover.src = article.image;
+        card.appendChild(cardCover);
 
-      // wrap cardTitleWrapper and cardActionWrapper in cardContentWrapper
-      cardContentWrapper.appendChild(cardTitleWrapper);
-      cardContentWrapper.appendChild(cardActionWrapper);
+        // wrap cardTitleWrapper and cardActionWrapper in cardContentWrapper
+        cardContentWrapper.appendChild(cardTitleWrapper);
+        cardContentWrapper.appendChild(cardActionWrapper);
 
-      cardTitleWrapper.innerHTML += `
-        <h1>${article.title}</h1>
-        <p>${article.description}</p>
-      `;
+        cardTitleWrapper.innerHTML += `
+            <h1>${article.title}</h1>
+            <p>${article.description}</p>
+          `;
 
-      card.appendChild(cardContentWrapper);
+        card.appendChild(cardContentWrapper);
 
-      arrowIcon.src = "./assets/icons/arrow-right.svg";
-      card.appendChild(arrowIcon);
+        arrowIcon.src = "./assets/icons/arrow-right.svg";
+        card.appendChild(arrowIcon);
 
-      cardAction.href = "#";
-      cardAction.textContent = "კურსის დეტალები";
-      cardActionWrapper.appendChild(arrowIcon);
-      cardActionWrapper.appendChild(cardAction);
+        cardAction.href = "#";
+        cardAction.textContent = "კურსის დეტალები";
+        cardActionWrapper.appendChild(arrowIcon);
+        cardActionWrapper.appendChild(cardAction);
 
-      gridContainer.appendChild(card);
-    });
-  })
-  .catch((error) => console.error(error));
+        gridContainer.appendChild(card);
+      });
+    })
+    .catch((error) => console.error(error));
+});
 
-// ---------- carousel ---------- //
-const carousel = document.querySelector(".carousel");
-const slider = document.querySelector(".slider");
-const leftArrow = document.querySelector(".left");
-const rightArrow = document.querySelector(".right");
-const dotParent = document.querySelector(".controls ul");
-const slidesCount = slider.children.length - 1;
-let slideIndex = 0;
-let intervalId = 0;
-const slideInterval = 3000;
+// ---------- CAROUSEL ---------- //
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("./db/carousel-data.json")
+    .then((response) => response.json())
+    // create HTML elements and append to the slider
+    .then((data) => {
+      const slider = document.querySelector(".slider");
+      // counter to keep track of the number of articles within the current cardContainer
+      let articlesCounter = 0;
 
-const resetStyles = () => {
-  for (let i = 0; i < slider.children.length; i++) {
-    slider.children[i].style.zIndex = 0;
-    slider.children[i].style.opacity = 0;
+      data.forEach((partner) => {
+        // check if the current cardContainer is full (3 articles) if true create new cardContainer for the first article
+        if (articlesCounter === 0) {
+          const cardContainer = document.createElement("div");
+          cardContainer.classList.add("cards-container");
+          slider.appendChild(cardContainer);
+        }
+
+        // find the last cardContainer (the one just added)
+        const cardContainers = document.querySelectorAll(".cards-container");
+        const currentCardContainer = cardContainers[cardContainers.length - 1];
+
+        // create article and img elements
+        const article = document.createElement("article");
+        const img = document.createElement("img");
+        img.alt = partner.alt;
+        img.src = partner.src;
+
+        // append img to article and article to the current cardContainer
+        article.appendChild(img);
+        currentCardContainer.appendChild(article);
+
+        // increment the articles counter
+        articlesCounter++;
+
+        // if the current cardContainer is full, reset the counter
+        if (articlesCounter === 3) {
+          articlesCounter = 0;
+        }
+      });
+    })
+    // carousel functionality
+    .then(() => {
+      const carousel = document.querySelector(".carousel");
+      const slider = document.querySelector(".slider");
+      const leftArrow = document.querySelector(".left");
+      const rightArrow = document.querySelector(".right");
+      const dotParent = document.querySelector(".controls ul");
+      const slidesCount = slider.children.length - 1;
+      let slideIndex = 0;
+      let intervalId = 0;
+      const slideInterval = 3000;
+
+      const resetStyles = () => {
+        for (let i = 0; i < slider.children.length; i++) {
+          slider.children[i].style.zIndex = 0;
+          slider.children[i].style.opacity = 0;
+        }
+      };
+
+      const setIndexHandler = () => {
+        resetStyles();
+        document
+          .querySelector(".controls .selected-dot")
+          .classList.remove("selected-dot");
+        slider.children[slideIndex].style.zIndex = 1;
+        slider.children[slideIndex].style.opacity = 1;
+      };
+
+      document.querySelectorAll(".controls li").forEach((dot, index) => {
+        dot.addEventListener("click", () => {
+          slideIndex = index;
+          setIndexHandler();
+          dot.classList.add("selected-dot");
+        });
+      });
+
+      // auto fading
+      const startAnimating = () => {
+        intervalId = setInterval(() => {
+          resetStyles();
+          document
+            .querySelector(".controls .selected-dot")
+            .classList.remove("selected-dot");
+          slideIndex = slideIndex < slidesCount ? slideIndex + 1 : 0;
+          dotParent.children[slideIndex].classList.add("selected-dot");
+          slider.children[slideIndex].style.zIndex = 1;
+          slider.children[slideIndex].style.opacity = 1;
+        }, slideInterval);
+      };
+      startAnimating();
+
+      carousel.addEventListener("mouseover", () => {
+        clearInterval(intervalId);
+      });
+      carousel.addEventListener("mouseout", () => {
+        startAnimating();
+      });
+
+      // prev
+      leftArrow.addEventListener("click", () => {
+        resetStyles();
+        slideIndex = slideIndex > 0 ? slideIndex - 1 : 0;
+        setIndexHandler();
+        dotParent.children[slideIndex].classList.add("selected-dot");
+        slider.children[slideIndex].style.zIndex = 1;
+        slider.children[slideIndex].style.opacity = 1;
+      });
+      // next
+      rightArrow.addEventListener("click", () => {
+        resetStyles();
+        slideIndex = slideIndex < slidesCount ? slideIndex + 1 : slidesCount;
+        setIndexHandler();
+        dotParent.children[slideIndex].classList.add("selected-dot");
+        slider.children[slideIndex].style.zIndex = 1;
+        slider.children[slideIndex].style.opacity = 1;
+      });
+    })
+    .catch((error) => console.error("Error fetching partners:", error));
+});
+
+// ---------- DYNAMIC ANCHOR TAG ---------- //
+const collapseContainer = document.querySelector(".collapse-container");
+const titleWrapper = document.querySelector(
+  ".third-section-container-content-bottom-inner-title-wrapper"
+);
+const allQuestionsLink = document.getElementById("all-questions");
+
+if (windowWidth < 768) {
+  moveElement(allQuestionsLink, titleWrapper, collapseContainer);
+} else {
+  moveElement(allQuestionsLink, collapseContainer, titleWrapper);
+}
+
+function moveElement(element, source, destination) {
+  if (source.contains(element)) {
+    source.removeChild(element);
+    destination.appendChild(element);
   }
-};
+}
 
-const setIndexHandler = () => {
-  resetStyles();
-  document
-    .querySelector(".controls .selected-dot")
-    .classList.remove("selected-dot");
-  slider.children[slideIndex].style.zIndex = 1;
-  slider.children[slideIndex].style.opacity = 1;
-};
-
-document.querySelectorAll(".controls li").forEach((dot, index) => {
-  dot.addEventListener("click", () => {
-    slideIndex = index;
-    setIndexHandler();
-    dot.classList.add("selected-dot");
-  });
-});
-
-// auto fading
-const startAnimating = () => {
-  intervalId = setInterval(() => {
-    resetStyles();
-    document
-      .querySelector(".controls .selected-dot")
-      .classList.remove("selected-dot");
-    slideIndex = slideIndex < slidesCount ? slideIndex + 1 : 0;
-    dotParent.children[slideIndex].classList.add("selected-dot");
-    slider.children[slideIndex].style.zIndex = 1;
-    slider.children[slideIndex].style.opacity = 1;
-  }, slideInterval);
-};
-startAnimating();
-
-carousel.addEventListener("mouseover", () => {
-  clearInterval(intervalId);
-});
-carousel.addEventListener("mouseout", () => {
-  startAnimating();
-});
-
-// prev
-leftArrow.addEventListener("click", () => {
-  resetStyles();
-  slideIndex = slideIndex > 0 ? slideIndex - 1 : 0;
-  setIndexHandler();
-  dotParent.children[slideIndex].classList.add("selected-dot");
-  slider.children[slideIndex].style.zIndex = 1;
-  slider.children[slideIndex].style.opacity = 1;
-});
-// next
-rightArrow.addEventListener("click", () => {
-  resetStyles();
-  slideIndex = slideIndex < slidesCount ? slideIndex + 1 : slidesCount;
-  setIndexHandler();
-  dotParent.children[slideIndex].classList.add("selected-dot");
-  slider.children[slideIndex].style.zIndex = 1;
-  slider.children[slideIndex].style.opacity = 1;
-});
-
-// ---------- collapse functionality ---------- //
+// ---------- COLLAPSE FUNCTIONALITY ---------- //
 function collapseHandler(contentId) {
   const content = document.getElementById(contentId);
   const arrowDownIcon = content.previousElementSibling.querySelector("img");
